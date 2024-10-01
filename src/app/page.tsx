cc4882a67b8148e898dd266499f2ee65c0428151
ngc2207 <cfngc3109@gmail.com>
2024-10-01 18:34:06 +0800

    refactor: Refactor layout structure in page and layout provider components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import FAQs from "@/components/faqs";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 import { MainView } from "@/components/main-view";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <MainView />
       <FAQs />
+      <Footer />
     </>
   );
 }
--- src/providers/layout-provider.tsx
+++ src/providers/layout-provider.tsx
@@ -1,12 +1,7 @@
-import { Footer } from "@/components/footer";
-import { Header } from "@/components/header";
-
 export function LayoutProvider({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
       <main className="flex-auto">{children}</main>
-      <Footer/>
     </>
   );
 }
