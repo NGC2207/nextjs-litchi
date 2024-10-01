fea9b1ab8e04ee4283a3d2e0fc0551e8d520ef5f
ngc2207 <cfngc3109@gmail.com>
2024-10-01 18:36:35 +0800

    feat(dashboard): Add dashboard layout and page components

---
+++ src/app/(main)/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+import { Header } from "@/app/(main)/dashboard/components/header";
+import { Sidebar } from "@/app/(main)/dashboard/components/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <div>
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="lg:pl-72">
+        <Header setSidebarOpen={setSidebarOpen} />
+        <main className="py-10">
+          <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+        </main>
+      </div>
+    </div>
+  );
+}
